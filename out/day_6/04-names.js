533a965f8db941e7455aad0a7e51e8dcc23e7fa1
Naitwa Alexandre <naitwa-alexandre@gmail.com>
2021-10-08 08:43:56 -0300

    first class out 08

---
+++ out/day_6/04-names.js
@@ -0,0 +1,7 @@
+const SUPER_SECRET = 'SUPER_SECRET';
+
+const jhon = 'jhon';
+const alexandre = 'alexandre';
+const karen = 'karen';
+
+module.exports = { alexandre, jhon, karen };
---
+++ out/day_6/App.js
@@ -0,0 +1,6 @@
+const names = require('./04-names');
+
+console.log(names);
+// sayHi('Karen');
+// sayHi(alexandre);
+// sayHi(jhon);
